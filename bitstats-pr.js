21734770132ac89aa8b8418ac5965fb7ab15e0f6
Matthew Crumley <matt@unsaturated.com>
2017-06-09 14:49:30 -0400

    Unify terminology - use 'pull request'

--- bitstats-pr.js
+++ bitstats-pr.js
@@ -2,10 +2,9 @@
  * Entry point for the bitstats 'pr' command.
  */
 const program = require('commander');
-const setup = require('./bitstats-setup/setup');
 
 program
-  .description('Gets peer review information from a specific repository.')
+  .description('Gets pull request information from a specific repository.')
   .command('index', 'creates or removes the PR index')
   .usage('[options] [repository-slug]')
   .parse(process.argv);
